df2c043486998356e6b150eac97318db25221a24
Alban Rahier <arahier@greenspector.com>
2022-11-10 19:28:40 +0100

    :closed_lock_with_key: add an admin key
    DO NOT USE IN PRODUCTION

--- database/database.go
+++ database/database.go
@@ -8,42 +8,22 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-// TODO retrieve it from secret config
-var adminPassword, _ = bcrypt.GenerateFromPassword([]byte("57aba9df-969f-4871-a095-e916d06ba38b"), bcrypt.DefaultCost)
-
-const schema = `--sql
-	CREATE TABLE IF NOT EXISTS accounts (
-		id serial PRIMARY KEY,
-		name VARCHAR ( 20 ) UNIQUE NOT NULL,
-		password VARCHAR ( 60 ) UNIQUE NOT NULL,
-		admin BOOLEAN NOT NULL DEFAULT FALSE,
-		created_on TIMESTAMP NOT NULL
-	);
-	CREATE TABLE IF NOT EXISTS geopoints (
-		id serial PRIMARY KEY,
-		title VARCHAR ( 30 ) NOT NULL,
-		user_id serial NOT NULL,
-		location geography ( POINT , 4326 ),
-		created_on TIMESTAMP NOT NULL,
-		amplitudes FLOAT [],
-		picture VARCHAR ( 42 ) NOT NULL,
-		sound VARCHAR ( 42 ) NOT NULL,
-		available BOOLEAN NOT NULL DEFAULT FALSE
-	);`
-
 /*const addIndexing = `--sql
 CREATE INDEX idx_geopoints_geom ON geopoints USING gist ((location));
 `*/ //creates a gist index to help location research (to use ?)
 
+const adminKey = "admin.key"
+
 func InitDb() (*sqlx.DB, error) {
-	fmt.Println(os.Getenv("DATABASE_URL"))
 	db, err := sqlx.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		return db, fmt.Errorf("error opening database: %q", err)
 	}
 
-	db.MustExec(schema)
-	db.MustExec("INSERT INTO accounts (name, created_on, password, admin) VALUES ($1,now(),$2,'t') ON CONFLICT DO NOTHING", "admin", adminPassword)
+	adminPassword := os.Getenv("SECRETS_FOLDER") + string(os.PathSeparator) + adminKey
+	adminHash, _ := bcrypt.GenerateFromPassword([]byte(adminPassword), bcrypt.DefaultCost)
+	db.MustExec(initTables)
+	db.MustExec(createAdmin, "admin", adminHash)
 
 	return db, nil
 }
--- database/query.go
+++ database/query.go
@@ -1,6 +1,33 @@
 package database
 
 const (
+	initTables = `--sql
+		CREATE TABLE IF NOT EXISTS accounts (
+			id serial PRIMARY KEY,
+			name VARCHAR ( 20 ) UNIQUE NOT NULL,
+			password VARCHAR ( 60 ) UNIQUE NOT NULL,
+			admin BOOLEAN NOT NULL DEFAULT FALSE,
+			created_on TIMESTAMP NOT NULL
+		);
+		CREATE TABLE IF NOT EXISTS geopoints (
+			id serial PRIMARY KEY,
+			title VARCHAR ( 30 ) NOT NULL,
+			user_id serial NOT NULL,
+			location geography ( POINT , 4326 ),
+			created_on TIMESTAMP NOT NULL,
+			amplitudes FLOAT [],
+			picture VARCHAR ( 42 ) NOT NULL,
+			sound VARCHAR ( 42 ) NOT NULL,
+			available BOOLEAN NOT NULL DEFAULT FALSE
+		);
+	`
+
+	createAdmin = `--sql
+		INSERT INTO accounts (name, created_on, password, admin) 
+		VALUES ($1,now(),$2,'t') 
+		ON CONFLICT DO NOTHING
+	`
+
 	GetClosestGeoId = `--sql
 		WITH excluded(id) AS ( SELECT UNNEST($2::int[])) 
 		SELECT geo.id FROM geopoints geo 
@@ -8,6 +35,7 @@ const (
 		ORDER BY geo.location <-> GeomFromEWKB($1)
 		LIMIT 1;
 	`
+
 	GetGeoPoint = `--sql
 		SELECT * FROM geopoints WHERE id = $1
 	`
