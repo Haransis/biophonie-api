1ddb95e8c47427b90a2459e53e3884469963ff87
Alban Rahier <arahier@greenspector.com>
2022-11-18 22:18:42 +0100

    :sparkles: add a web map display

---
+++ public/web/scripts/mapbox.js
@@ -0,0 +1,49 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiaGFyYW5zaXMiLCJhIjoiY2thd2FqZWozMXJuczJ4cWZycHo2Njl6MyJ9.JEmqtXLcoWK1euOnKJvxrQ';
+const baseUrl = window.location.protocol + '//' + window.location.host + '/api/v1/assets/'
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/geoffreydorne/cjy30xoii1tid1crv9bz19hct',
+    center: [14.012033, 49.245659],
+    zoom: 4,
+});
+map.on('load', () => {
+    map.loadImage('icons/ic_marker.png',
+    (error,image) => {
+        if (error) throw error;
+
+        map.addImage('geopoints-image', image);
+        map.addSource('geopoints-source', {
+            type: 'geojson',
+            data: baseUrl + 'geojson.json'
+        });
+        map.addLayer({
+            id: 'geopoints-layer',
+            type: 'symbol',
+            source: 'geopoints-source',
+            'layout': {
+                'icon-size': 0.35,
+                'icon-image': 'geopoints-image',
+                'text-field': ['get', 'name'],
+                'text-font': [
+                    'IBM Plex Mono Regular',
+                    'IBM Plex Mono Bold'
+                ],
+                'text-size': 14,
+                'text-offset': [1, 0],
+                'text-anchor': 'left'
+            }
+        });
+
+        map.on('click', 'geopoints-layer', (e) => {
+            map.flyTo({
+                center: e.features[0].geometry.coordinates
+            });
+        });
+        map.on('mouseenter', 'geopoints-layer', () => {
+            map.getCanvas().style.cursor = 'pointer';
+        });
+        map.on('mouseleave', 'geopoints-layer', () => {
+            map.getCanvas().style.cursor = '';
+        });
+    })
+});
